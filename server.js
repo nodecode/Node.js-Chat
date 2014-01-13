908e0b0182a2f83252412e4eb3a582baddf20013
benutzer <benutzer@146-52-128-57-dynip.superkabel.de>
2014-01-13 10:53:56 +0100

    new

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express')
+,   app = express()
+,   server = require('http').createServer(app)
+,   io = require('socket.io').listen(server)
+,   conf = require('./config.json');
+
+// Webserver
+// auf den Port x schalten
+server.listen(conf.port);
+
+app.configure(function(){
+	// statische Datein ausliefern
+	app.use(express.static(__dirname + '/public'));
+});
+
+// wenn der Pfad / aufgerufen wird
+app.get('/', function (req, res) {
+	// so wird die Datei index.html ausgegeben
+	res.sendfile(__dirname + '/public/index.html');
+});
+
+// Websocket
+io.sockets.on('connection', function (socket) {
+	// der Client ist verbunden
+	socket.emit('chat', { zeit: new Date(), text: 'Du bist nun mit dem Server verbunden!' });
+	// wenn ein Benutzer einen Text senden
+	socket.on('chat', function (data) {
+		// so wird dieser Text an alle anderen Benutzer gesendet
+		socket.emit('chat', { zeit: new Date(), name: data.name || 'Anonym', text: data.text });
+	});
+});
+
+// Portnummer in die Konsole schreiben
+console.log('Der Server läuft nun auf dem Port ' + conf.port);
